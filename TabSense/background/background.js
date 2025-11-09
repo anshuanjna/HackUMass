41778c8701cbecab25f3add540aaa09bf6fd8979
Pihul Nyati <pnyati@umass.edu>
2025-11-09 01:48:39 -0500

    chatbot

--- TabSense/background/background.js
+++ TabSense/background/background.js
@@ -35,4 +35,3 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
 // Run a full grouping when the extension is first installed
 chrome.runtime.onInstalled.addListener(groupAllTabs);
-chrome.runtime.sendMessage({ action: "GROUP_TABS" });
